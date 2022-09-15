6d02e201617d9df93ceaa093f98cf620bdbd0434
sivaranjanijayanthi <113495188+sivaranjanijayanthi@users.noreply.github.com>
2022-09-14 20:35:16 -0700

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,13 @@
+package find.area;
+
+public class Circle {
+	public static void main(String args[]) {
+		int r=9;
+		double p=3.14;
+		double tot;
+		tot=p*r*r;
+		System.out.println("The area of circle is:\n" + tot);
+		
+	}
+
+}
---
+++ Triangle.java
@@ -0,0 +1,12 @@
+package find.area;
+
+public class Triangle {
+	public static void main( String args[]) {
+		int b=8; 
+		int h=20;
+		float area;
+		area=(b*h)/2;
+		System.out.println("The Area of triangle is:\n" + area);
+	}
+
+}
