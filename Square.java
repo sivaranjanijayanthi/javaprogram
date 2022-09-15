4f513a749465a9deca7adde01786bdd4d2c1859d
sivaranjanijayanthi <113495188+sivaranjanijayanthi@users.noreply.github.com>
2022-09-14 20:34:37 -0700

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,12 @@
+package find.area;
+
+public class Square {
+	public static void main(String args[]) {
+		int a=10;
+		float area;
+		area =2*a;
+		System.out.println("The square of area is:\n" + area);
+		
+	}
+
+}
