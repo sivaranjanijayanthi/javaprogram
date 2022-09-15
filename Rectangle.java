31a1ddd373e05e926d3375d2e353ae48eddb2e0c
sivaranjanijayanthi <113495188+sivaranjanijayanthi@users.noreply.github.com>
2022-09-14 20:33:20 -0700

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,15 @@
+package find.area;
+
+public class Rectangle 
+{
+	public static void main (String []args) {
+		int l,b,area;
+		l=15;
+		b=6;
+		area=l*b;
+		System.out.println("The area of Rectangle is:\n"  + area);
+		
+		
+	}
+
+}
